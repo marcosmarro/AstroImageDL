53d914d49662fa3ba92f498ab230c25f61d4ebb2
marcosmarro <marroquinmarcos677@gmail.com>
2025-09-12 23:43:09 -0700

    Fixed run_all subprocess commands to strings

--- run_all.py
+++ run_all.py
@@ -5,24 +5,24 @@ models = ['original', 'n2v', 'calibrated', 'n2n']
 
 # Train
 print("Training N2V model...")
-subprocess.run(["python train.py -d Training -m n2v"], check=True, shell=True)
+subprocess.run("python train.py -d Training -m n2v", check=True, shell=True)
 
 print("Training N2N model...")
-subprocess.run(["python train.py -d Training -m n2n"], check=True, shell=True)
+subprocess.run("python train.py -d Training -m n2n", check=True, shell=True)
 
 # Inference
 print("Performing N2V inference...")
-subprocess.run(["python inference.py -d Science -m n2v"], check=True, shell=True)
+subprocess.run("python inference.py -d Science -m n2v", check=True, shell=True)
 
 print("Performing N2N inference...")
-subprocess.run(["python inference.py -d Science -m n2n"], check=True, shell=True)
+subprocess.run("python inference.py -d Science -m n2n", check=True, shell=True)
 
 # Evaluation
 print("Evaluating N2V results...")
-subprocess.run(["python evaluation.py -d DenoisedScience -m n2v"], check=True, shell=True)
+subprocess.run("python evaluation.py -d DenoisedScience -m n2v", check=True, shell=True)
 
 print("Evaluating N2N results...")
-subprocess.run(["python evaluation.py -d DenoisedScience -m n2n"], check=True, shell=True)
+subprocess.run("python evaluation.py -d DenoisedScience -m n2n", check=True, shell=True)
 
 # Plotting
 print("Generating plots...")
